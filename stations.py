b202149e1ac4a712a6c4f9f391faab202de094c7
Harold Cooper <hrldcpr@gmail.com>
2013-07-10 21:49:02 -0400

    script for getting station metadata

---
+++ stations.py
@@ -0,0 +1,13 @@
+import json
+import sys
+
+import requests
+
+KEYS = {'id', 'label', 'latitude', 'longitude'}
+
+stations = requests.get('http://appservices.citibikenyc.com/data2/stations.php').json()['results']
+stations = [{k: s[k] for k in KEYS} for s in stations]
+
+sys.stdout.write('var STATIONS = ')
+json.dump(stations, sys.stdout)
+sys.stdout.write(';\n')
