814dafac5c14150a08214165a43da4fcbb17c15d
Harold Cooper <hrldcpr@gmail.com>
2013-07-14 23:47:32 -0400

    more robust dump script

--- dump.py
+++ dump.py
@@ -3,47 +3,61 @@ import datetime
 import gzip
 import io
 import json
+import logging
 import operator
 import os
 import sys
+
+
+START = int(sys.argv[1]);
+DELTA = int(sys.argv[2]) * 60
+SUFFIX = '.json.gz'
 
 
 def xy(a, b):
     return {'x': a - b,
             'y': b - a}
 
-DELTA = 60 * 60
-SUFFIX = '.json.gz'
-
 os.chdir('stations2')
 paths = sorted(p for p in os.listdir() if p.endswith(SUFFIX))
 
-latest = None
-start = None
+latest = START - DELTA
 stations = []
 matrix = []
 for path in paths:
     t = int(path[:-len(SUFFIX)])
-    if latest is None or t - latest >= DELTA:
-        assert latest is None or t - latest < 2 * DELTA, '{path} follows a gap of more than {delta} seconds'.format(path=path, delta=DELTA)
+    if t - latest >= DELTA:
+        assert t - latest < 2 * DELTA, '{path} follows a gap of more than {delta} seconds'.format(path=path, delta=DELTA)
+        latest += DELTA
+
         with io.TextIOWrapper(gzip.open(path)) as f:
             doc = json.load(f)
-        print('{t} / {x} seconds old / {n}'.format(t=datetime.datetime.fromtimestamp(t), x=t - doc['lastUpdate'], n=len(matrix)),
-              file=sys.stderr)
+        logging.info('{t} / {x} seconds old / {n}'.format(t=datetime.datetime.fromtimestamp(t), x=t - doc['lastUpdate'], n=len(matrix)))
 
         row = []
-        for i,station in enumerate(sorted(doc['results'], key=operator.itemgetter('id'))):
-            if len(stations) <= i: stations.append(station['id'])
-            assert stations[i] == station['id'], '{path} contains a different set of stations: {x} vs {y}'.format(path=path, **xy({x['id'] for x in doc['results']}, set(stations)))
+        i = 0
+        for station in sorted(doc['results'], key=operator.itemgetter('id')):
+            while i < len(stations) and stations[i] != station['id']:
+                if stations[i] > station['id']:
+                    # a new station that's not at the end, so we have to fix all previous rows
+                    logging.warning('{path} contains a different set of stations: {x} vs {y}'.format(path=path, **xy({x['id'] for x in doc['results']}, set(stations))))
+                    stations.insert(i, station['id'])
+                    for old_row in matrix:
+                        old_row.insert(i, [0, 0])
+                    continue
+                logging.warning('{path} is missing station {id}'.format(path=path, id=station['id']))
+                row.append([0, 0])
+                i += 1
+            if i >= len(stations): stations.append(station['id'])
             row.append([station['availableBikes'], station['availableDocks']])
+            i += 1
+
         assert len(row) == len(stations)
         matrix.append(row)
 
-        latest = t
-        if start is None: start = t
 
 sys.stdout.write('var DATA = ')
-json.dump({'start': start,
+json.dump({'start': START,
            'delta': DELTA,
            'stations': stations,
            'data': matrix},
